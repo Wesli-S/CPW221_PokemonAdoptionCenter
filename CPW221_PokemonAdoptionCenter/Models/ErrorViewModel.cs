56b86594999d67f4b3b43528b21ee24ab2578bc6
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-04-06 10:32:25 -0700

    Creating the repository

---
+++ CPW221_PokemonAdoptionCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CPW221_PokemonAdoptionCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
