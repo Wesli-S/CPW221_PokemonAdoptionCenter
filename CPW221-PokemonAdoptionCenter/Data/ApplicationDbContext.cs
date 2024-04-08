f92e995281ca444d484a2b685d7d43b8adb36dad
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-04-08 13:18:11 -0700

    Deleted original project and remade it with Identity

---
+++ CPW221-PokemonAdoptionCenter/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPW221_PokemonAdoptionCenter.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
