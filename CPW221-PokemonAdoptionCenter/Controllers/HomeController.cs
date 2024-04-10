5d00529520b057e74d9b2be58dc4359cdb2a2fbe
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-04-10 16:25:04 -0700

    deleted AboutUs.cshtml due to me creating it wrong

--- CPW221-PokemonAdoptionCenter/Controllers/HomeController.cs
+++ CPW221-PokemonAdoptionCenter/Controllers/HomeController.cs
@@ -27,11 +27,6 @@ namespace CPW221_PokemonAdoptionCenter.Controllers
 
         //User MUST make an account before applying to volunteer/adopt
 
-        public IActionResult AboutUs()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
