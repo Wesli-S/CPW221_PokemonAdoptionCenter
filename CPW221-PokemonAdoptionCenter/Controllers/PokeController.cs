734ddd1c6b9733504cf07acea90c2d77d27389d8
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-04-10 18:58:10 -0700

    Created About Us page

--- CPW221-PokemonAdoptionCenter/Controllers/PokeController.cs
+++ CPW221-PokemonAdoptionCenter/Controllers/PokeController.cs
@@ -1,8 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW221_PokemonAdoptionCenter.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW221_PokemonAdoptionCenter.Controllers
 {
     public class PokeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>All Pokemon currently in the Adoption Center</returns>
+        public async Task<IActionResult> AvailableMons() 
+        { 
+            //Get all Pokemon from the DB
+            /*List<Pokemon> adopt = await (from pokemon in _context.Pokemon
+             *                          select pokemon).ToListAsync(); //FINISH LATER*/
+
+            //Show them on the page
+            return View(/*pokemon*/);
+        }
+
+        public async Task<IActionResult> AboutUs() 
+        {
+
+            return View();
+        }
     }
 }
