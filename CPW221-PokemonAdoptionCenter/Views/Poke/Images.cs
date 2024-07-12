2199911f40d16c62f567be88dfca8fde1d2419e0
Wesli-S <146999017+Wesli-S@users.noreply.github.com>
2024-07-12 12:17:59 -0700

    Added Images class

---
+++ CPW221-PokemonAdoptionCenter/Views/Poke/Images.cs
@@ -0,0 +1,14 @@
+﻿namespace CPW221_PokemonAdoptionCenter.Views.Poke
+{
+    public class Images
+    {
+        public async Task<byte[]> GetByteArrayFromImage(IFormFile image)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                await image.CopyToAsync(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
